fac56c1aff3ccaf25ab92ac1f907c6cba048c5bc
Yuuki Satou <yuukisatou@YuukinoMacBook-Air.local>
2026-05-06 07:24:42 +0900

    fix: 外部リンクを新しいタブで開く設定を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypeExternalLinks from 'rehype-external-links';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-	site: 'https://gaichuu-navi.com',
+	site: 'https://gaichuunavi.com',
 	integrations: [mdx(), sitemap()],
 	output: 'static',
+	markdown: {
+		rehypePlugins: [
+			[rehypeExternalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }],
+		],
+	},
 });
